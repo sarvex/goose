be8ab2985ba242ff71883aa7c740a0612b4a492e
Mike Fridman <mf192@icloud.com>
2024-04-13 17:20:06 -0400

    Use mfridman/buildversion

--- cmd/goose/main.go
+++ cmd/goose/main.go
@@ -10,14 +10,13 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"runtime/debug"
 	"sort"
 	"strconv"
 	"strings"
 	"text/tabwriter"
 	"text/template"
-	"time"
 
+	"github.com/mfridman/buildversion"
 	"github.com/pressly/goose/v3"
 	"github.com/pressly/goose/v3/internal/cfg"
 	"github.com/pressly/goose/v3/internal/cli"
@@ -45,7 +44,7 @@ var version string
 
 func main() {
 	if ok, err := strconv.ParseBool(os.Getenv("GOOSE_CLI")); err == nil && ok {
-		cli.Main(cli.WithVersion(getVersionFromBuildInfo()))
+		cli.Main(cli.WithVersion(buildversion.New(version)))
 		return
 	}
 	ctx := context.Background()
@@ -57,7 +56,7 @@ func main() {
 	}
 
 	if *versionFlag {
-		fmt.Printf("goose version: %s\n", getVersionFromBuildInfo())
+		fmt.Printf("goose version: %s\n", buildversion.New(version))
 		return
 	}
 	if *verbose {
@@ -402,68 +401,4 @@ func printValidate(filename string, verbose bool) error {
 		)
 	}
 	return w.Flush()
-}
-
-// getVersionFromBuildInfo returns the version string from the build info, if available. It will
-// always return a non-empty string.
-//
-//   - If the version is set explicitly, it returns the string as is.
-//   - If the build info is not available, it returns "devel".
-//   - If building from source, it returns "devel" followed by the first 12 characters of the VCS
-//     revision, followed by ", dirty" if the working directory was dirty. For example,
-//     "devel (abcdef012345, dirty)" or "devel (abcdef012345)". If the VCS revision is not available,
-//     "unknown revision" is used instead.
-//
-// Note, vcs info not stamped when built listing .go files directly. E.g.,
-//   - `go build main.go`
-//   - `go build .`
-//
-// For more information, see https://github.com/golang/go/issues/51279
-func getVersionFromBuildInfo() (out string) {
-	defer func() {
-		out = strings.TrimSpace(out)
-	}()
-	if version != "" {
-		return version
-	}
-	const defaultVersion = "devel"
-
-	buildInfo, ok := debug.ReadBuildInfo()
-	if !ok {
-		// Should only happen if -buildvcs=false is set or using a really old version of Go.
-		return defaultVersion
-	}
-	// The (devel) string is not documented, but it is the value used by the Go toolchain. See
-	// https://github.com/golang/go/issues/29228
-	if s := buildInfo.Main.Version; s != "" && s != "(devel)" {
-		return buildInfo.Main.Version
-	}
-	var vcs struct {
-		revision string
-		time     time.Time
-		modified bool
-	}
-	for _, setting := range buildInfo.Settings {
-		switch setting.Key {
-		case "vcs.revision":
-			vcs.revision = setting.Value
-		case "vcs.time":
-			vcs.time, _ = time.Parse(time.RFC3339, setting.Value)
-		case "vcs.modified":
-			vcs.modified = (setting.Value == "true")
-		}
-	}
-	var b strings.Builder
-	b.WriteString(defaultVersion)
-	b.WriteString(" (")
-	if vcs.revision == "" || len(vcs.revision) < 12 {
-		b.WriteString("unknown revision")
-	} else {
-		b.WriteString(vcs.revision[:12])
-	}
-	if vcs.modified {
-		b.WriteString(", dirty")
-	}
-	b.WriteString(")")
-	return b.String()
 }
